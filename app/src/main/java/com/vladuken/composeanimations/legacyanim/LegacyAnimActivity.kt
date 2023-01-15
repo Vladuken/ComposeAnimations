c98a773811652b102e1157520d584035211d1d78
Andrei <andreyrabo@mail.ru>
2023-01-15 18:49:47 +0000

    left right anim

--- app/src/main/java/com/vladuken/composeanimations/legacyanim/LegacyAnimActivity.kt
+++ app/src/main/java/com/vladuken/composeanimations/legacyanim/LegacyAnimActivity.kt
@@ -1,15 +1,8 @@
 package com.vladuken.composeanimations.legacyanim
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.view.animation.LinearInterpolator
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.math.MathUtils
 import com.vladuken.composeanimations.R
 
 class LegacyAnimActivity : AppCompatActivity() {
@@ -18,84 +11,21 @@ class LegacyAnimActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_legacy_anim)
 
-        val button = findViewById<Button>(R.id.button_change_size)
-        val view = findViewById<View>(R.id.rect)
-        val root = findViewById<View>(R.id.root)
+        val bigSmall = findViewById<Button>(R.id.button_big_small)
+        val leftRight = findViewById<Button>(R.id.button_left_right)
 
-        root.setBackgroundColor(Color.WHITE)
-
-        val density = resources.displayMetrics.density
-        val startPx = 100f * density
-        val endPx = 250f * density
-
-        var isSmall = true
-        button.text = if (isSmall) "make big" else "make small"
-
-        button.setOnClickListener {
-
-            var animator: ValueAnimator? = null
-
-            if (isSmall) {
-                isSmall = false
-
-                animator?.cancel()
-                animator?.removeAllUpdateListeners()
-
-                animator =
-                    ObjectAnimator.ofFloat(0f, 1f)
-                        .setDuration(1000L)
-
-                animator.interpolator = LinearInterpolator()
-                animator.addUpdateListener {
-
-                    val cur = view.width
-
-                    val fraction = it.animatedFraction
-                    val size = MathUtils.lerp(cur.toFloat(), endPx, fraction)
-
-                    val params = view.layoutParams as ConstraintLayout.LayoutParams
-                    params.width = size.toInt()
-                    params.height = size.toInt()
-
-                    view.layoutParams = params
-                }
-
-                animator.start()
-
-
-            } else {
-                isSmall = true
-
-                animator?.cancel()
-                animator?.removeAllUpdateListeners()
-
-                animator =
-                    ObjectAnimator.ofFloat(0f, 1f)
-                        .setDuration(1000L)
-
-                animator.interpolator = LinearInterpolator()
-                animator.addUpdateListener {
-
-                    val cur = view.width
-
-                    val fraction = it.animatedFraction
-                    val size = MathUtils.lerp(cur.toFloat(), startPx, fraction)
-
-                    val params = view.layoutParams as ConstraintLayout.LayoutParams
-                    params.width = size.toInt()
-                    params.height = size.toInt()
-
-                    view.layoutParams = params
-                }
-
-                animator.start()
-
-            }
-
-
-            button.text = if (isSmall) "make big" else "make small"
+        bigSmall.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, BigSmallFragment())
+                .addToBackStack(null)
+                .commit()
         }
 
-
+        leftRight.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LeftRightFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
