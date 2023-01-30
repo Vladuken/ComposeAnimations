ca1e9d6caac0a51bb1c2249245c5b5f864e509fb
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2023-01-30 01:07:51 +0100

    Improve AnimationDebugString

--- app/src/main/java/com/vladuken/composeanimations/animation/Atoms.kt
+++ app/src/main/java/com/vladuken/composeanimations/animation/Atoms.kt
@@ -5,32 +5,16 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -126,20 +110,42 @@ fun AnimationSpot(
 fun AnimationDebugString(
     modifier: Modifier = Modifier,
     @FloatRange(from = 0.0, to = 1.0) fraction: Float,
-    text: String
+    text: String? = null,
+    baserFraction: Float = 0.7f,
 ) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp)
+            .padding(vertical = 4.dp),
     ) {
-        Text(text = text)
-        Spacer(
-            modifier = Modifier
-                .background(Color.Gray)
-                .height(8.dp)
-                .fillMaxWidth(fraction)
-        )
+        if (text != null) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth(baserFraction)
+                    .height(4.dp)
+                    .alpha(0.2f)
+                    .background(Color.Gray)
+            )
+            Card(
+                modifier = Modifier
+                    .size(16.dp)
+                    .offset(x = maxWidth * fraction * baserFraction),
+                shape = RoundedCornerShape(4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.DarkGray
+                )
+            ) {}
+        }
     }
 }
 
