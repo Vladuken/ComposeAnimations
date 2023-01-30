d94b35da10fccd3366a0af1273f7d7fcad65cb68
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2023-01-30 02:10:49 +0100

    EasingGraph

--- app/src/main/java/com/vladuken/composeanimations/animation/AnimationSpec.kt
+++ app/src/main/java/com/vladuken/composeanimations/animation/AnimationSpec.kt
@@ -1,46 +1,32 @@
 package com.vladuken.composeanimations.animation
 
-import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.repeatable
-import androidx.compose.animation.core.snap
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun AnimationSpecScreen() {
-    Column {
+    Column(
+        modifier = Modifier.padding(
+            horizontal = 32.dp,
+            vertical = 64.dp
+        )
+    ) {
         var isEnabled by remember { mutableStateOf(false) }
         val fraction = remember(isEnabled) { if (isEnabled) 1f else 0f }
 
@@ -68,8 +54,9 @@ fun AnimationSpecScreen() {
             )
         }
 
+        Spacer(modifier = Modifier.height(32.dp))
 
-        val duration = 1000
+        val duration = 1500
         // Tween
         AnimationSpecExample(
             fraction = fraction,
@@ -77,93 +64,93 @@ fun AnimationSpecScreen() {
         )
         AnimationSpecExample(
             fraction = fraction,
-            animationSpec = tween(duration, 100)
+            animationSpec = tween(duration, easing = LinearEasing)
         )
-
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = tween(
-                durationMillis = duration,
-                easing = LinearEasing
-            )
-        )
-
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = tween(
-                durationMillis = duration,
-                easing = FastOutSlowInEasing
-            )
-        )
-
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = tween(
-                durationMillis = duration,
-                easing = CubicBezierEasing(0.61f, -0.79f, 0.73f, 1.59f)
-            )
-        )
-
-        Spacer(
-            modifier = Modifier
-                .height(16.dp)
-                .background(Color.Black)
-        )
-        // Spring
-        val reallySlowStiffness = 15f
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = spring(
-                stiffness = reallySlowStiffness
-            )
-        )
-
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioHighBouncy,
-                stiffness = reallySlowStiffness
-            )
-        )
-
-        //keyframes
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = keyframes {
-                val durationStep = 10
-                durationMillis = durationStep * 100
-                0.0f at 0 with LinearOutSlowInEasing // for 0-15 ms
-                0.4f at durationStep * 10 // ms
-                0.6f at durationStep * 35 // ms
-                0.2f at durationStep * 60 // ms
-                0.6f at durationStep * 80 // ms
-            }
-        )
-        // Repeatable
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = repeatable(
-                iterations = 5,
-                animation = tween(1000)
-            )
-        )
-
-        // TODO Сделать что-то с infiniteRepeatable
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = infiniteRepeatable(
-                animation = tween(1000),
-                repeatMode = RepeatMode.Restart
-            )
-        )
-
-        // Snap
-        AnimationSpecExample(
-            fraction = fraction,
-            animationSpec = snap(
-                delayMillis = 1000
-            )
-        )
+//
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = tween(
+//                durationMillis = duration,
+//                easing = LinearEasing
+//            )
+//        )
+//
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = tween(
+//                durationMillis = duration,
+//                easing = FastOutSlowInEasing
+//            )
+//        )
+//
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = tween(
+//                durationMillis = duration,
+//                easing = CubicBezierEasing(0.61f, -0.79f, 0.73f, 1.59f)
+//            )
+//        )
+//
+//        Spacer(
+//            modifier = Modifier
+//                .height(16.dp)
+//                .background(Color.Black)
+//        )
+//        // Spring
+//        val reallySlowStiffness = 15f
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = spring(
+//                stiffness = reallySlowStiffness
+//            )
+//        )
+//
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = spring(
+//                dampingRatio = Spring.DampingRatioHighBouncy,
+//                stiffness = reallySlowStiffness
+//            )
+//        )
+//
+//        //keyframes
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = keyframes {
+//                val durationStep = 10
+//                durationMillis = durationStep * 100
+//                0.0f at 0 with LinearOutSlowInEasing // for 0-15 ms
+//                0.4f at durationStep * 10 // ms
+//                0.6f at durationStep * 35 // ms
+//                0.2f at durationStep * 60 // ms
+//                0.6f at durationStep * 80 // ms
+//            }
+//        )
+//        // Repeatable
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = repeatable(
+//                iterations = 5,
+//                animation = tween(1000)
+//            )
+//        )
+//
+//        // TODO Сделать что-то с infiniteRepeatable
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = infiniteRepeatable(
+//                animation = tween(1000),
+//                repeatMode = RepeatMode.Restart
+//            )
+//        )
+//
+//        // Snap
+//        AnimationSpecExample(
+//            fraction = fraction,
+//            animationSpec = snap(
+//                delayMillis = 1000
+//            )
+//        )
     }
 }
 
@@ -177,14 +164,66 @@ private fun AnimationSpecExample(
         targetValue = fraction,
         animationSpec = animationSpec,
     )
-    AnimationDebugString(
-        modifier = modifier,
-        fraction = fraction,
-        text = "Current Fraction: $fraction"
-    )
+    Spacer(modifier = Modifier.height(8.dp))
+
+    Column {
+        AnimationDebugString(
+            modifier = modifier,
+            fraction = fraction,
+        )
+        if (animationSpec is TweenSpec) {
+            EasingGraph(
+                easing = animationSpec.easing,
+                currentFraction = fraction
+            )
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun EasingGraph(
+    modifier: Modifier = Modifier,
+    easing: Easing,
+    currentFraction: Float
+) {
+    val path by remember { mutableStateOf(Path()) }
+    Canvas(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(2f)
+    ) {
+        // Draw Axis
+        path.reset()
+        path.lineTo(0f, size.height)
+        path.lineTo(size.width, size.height)
+        drawPath(
+            path = path,
+            color = Color.LightGray,
+            style = Stroke(width = 2f),
+        )
+
+        // Draw function
+        path.reset()
+        path.moveTo(0f, size.height)
+        generateSequence(0f) { it + 0.01f }
+            .takeWhile { it <= currentFraction }
+            .forEach {
+                path.lineTo(size.width * it, size.height - size.height * easing.transform(it))
+            }
+
+        drawPath(
+            path = path,
+            color = Color.Blue,
+            style = Stroke(width = 3f),
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0x00_FF_FF_FF
+)
 @Composable
 fun AnimationSpecScreenPreview() {
     AnimationSpecScreen()
